41fe713917dec81db9df5146a8e72db2a0c1c929
Quan0414 <quanthcstd@gmail.com>
2026-04-02 16:16:45 +0700

    feat: add logger

--- src/app.js
+++ src/app.js
@@ -10,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Request Logger
+app.use((req, res, next) => {
+  console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url}`);
+  next();
+});
+
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
